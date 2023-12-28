12f9412febc69772f0ee9c8917dd57a7c452ac37
Vaishnavisurvi <118847897+Vaishnavisurvi@users.noreply.github.com>
2023-12-28 21:04:57 +0530

    Add files via upload

---
+++ roomsRoute.js
@@ -0,0 +1,68 @@
+const express=require("express");
+const router=express.Router();
+
+
+
+
+const Room=require('../models/room')
+
+
+router.get("/getallrooms",async(req,res)=>{
+try{
+    const rooms=await Room.find({})
+    return res.json({rooms});
+}
+
+   catch(error){
+    return res.status(400).json({message:error});
+
+   }
+
+
+});
+
+
+router.post("/getroombyid",async(req,res)=>{
+const roomid=req.body.roomid
+
+    try{
+        const room=await Room.findOne({_id:roomid})
+        return res.json({room});
+    }
+    
+       catch(error){
+        return res.status(400).json({message:error});
+    
+       }
+    
+    
+    });
+
+
+
+
+    router.post("/addroom",async(req,res)=>{
+        
+        
+            try{
+   
+                const newroom=new Room(req.body);
+           await newroom.save()
+           res.send("New Room Added Successfully")
+            }
+            
+               catch(error){
+                return res.status(400).json({error});
+            
+               }
+            
+            
+            });
+        
+
+
+
+
+
+
+module.exports=router;
